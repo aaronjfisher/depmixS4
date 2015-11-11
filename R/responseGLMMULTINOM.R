4c9f068542c0292ceda6bcd96aaf5eb772da9d27
koooee <nickkolegraff@gmail.com>
2015-11-11 00:36:16 -0800

    debugging

--- R/responseGLMMULTINOM.R
+++ R/responseGLMMULTINOM.R
@@ -91,13 +91,13 @@ setMethod("dens","MULTINOMresponse",
 setMethod("predict","MULTINOMresponse",
 	function(object) {
 		print("predict multinomial response we are identity")
-		print(object@x)
-		print(object@parameters$coefficients)
+		print(head(object@x))
+		print(head(object@parameters$coefficients))
 		if(object@family$link=="identity") object@x%*%object@parameters$coefficients
 		else {
 			print("predict multinomial response we are NOT identity")
-			print(object@x)
-			print(object@parameters$coefficients)
+			print(head(object@x))
+			print(head(object@parameters$coefficients))
 			object@family$linkinv(object@x%*%object@parameters$coefficients,base=object@family$base)
 		}
 	}
