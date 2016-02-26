8bd989e53a6b351bb0ec05408ab481500f33f76c
koooee <nickkolegraff@gmail.com>
2016-02-26 22:06:31 +0000

    added test for hmms

---
+++ tests/testHMM.R
@@ -0,0 +1,6 @@
+library(doMC)
+library(depmixS4)
+data(speed)
+mod1 <- depmix(list(rt~1,corr~1),data=speed,transition=~Pacc,nstates=2,family=list(gaussian(),multinomial()),ntimes=c(168,134,137))
+set.seed(3)
+fmod1 <- fit(mod1)
